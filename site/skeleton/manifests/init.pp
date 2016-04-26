675a3621afdbe2ae8d7217e3b4e717dcf5ccb928
sucram65 <sucram65@yahoo.com>
2016-04-26 11:16:46 -0700

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,1 +1,15 @@
-
+class skeleton {
+ file { '/etc/skeleton':
+   ensure => directory,
+   owner => 'root',
+   group => 'group',
+   mode => '0755',
+   }
+ file { '/etc/skel/.bashrc',
+   ensure => file,
+   owner => 'root',
+   group => 'root',
+   mode => '0644',
+   source => 'puppet:///modules/skeleton/bashrc',
+  }
+}
