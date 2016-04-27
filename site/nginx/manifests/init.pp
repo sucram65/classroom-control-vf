57bf2f2da24c11fa6e619a999d1e968c67f8904f
sucram65 <sucram65@yahoo.com>
2016-04-27 10:21:15 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,42 +1,32 @@
 class nginx {
- 
-  file {
-   ensure => file,
-   owner => 'root',
-   group => 'root',
-   mode => '0664',
- }
-  
-  package { 'nginx':
-    ensure => 'present',
- }
-  
- file { '/var/www':
-  ensure => directory,
+File {
+owner => 'root',
+group => 'root',
+mode => '0664',
 }
-
+package { 'nginx':
+ensure => present,
+}
+file { [ '/var/www', '/etc/nginx/conf.d' ]:
+ensure => directory,
+}
 file { '/var/www/index.html':
- source => 'puppet:///modules/nginx/index.html',
+ensure => file,
+source => 'puppet:///modules/nginx/index.html',
 }
-
 file { '/etc/nginx/nginx.conf':
- source => 'puppet:///modules/nginx/nginx.conf',
- require => Package['nginx'],
- notify => Service['nginx'],
+ensure => file,
+source => 'puppet:///modules/nginx/nginx.conf',
+require => Package['nginx'],
+notify => Service['nginx'],
 }
-
-file { '/etc/nginx/conf.d':
- ensure => directory,
-}
-
 file { '/etc/nginx/conf.d/default.conf':
- source => 'puppet:///modules/nginx/default.conf',
- require => Package['nginx'],
- notify => Service['nginx'],
+ensure => file,
+source => 'puppet:///modules/nginx/default.conf',
+notify => Service['nginx'],
+require => Package['nginx'],
 }
-
 service { 'nginx':
- ensure => running,
- enable => true,
-}
+ensure => running,
+enable => true,
 }
