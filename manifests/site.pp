7484daa48f102487427d8a3df7752ea371daebbc
sucram65 <sucram65@yahoo.com>
2016-04-28 09:56:52 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -47,11 +47,13 @@ node default {
 if $::virtual != 'physical' {
 $vmname = capitalize($::virtual)
 notify { "This is a ${vmname} virtual machine.": }
-}
 
 #Display messages from HIERA
 $message = hiera('message')
 notify { $message: }
+
+}
+
 
 #include users
 include memcached
